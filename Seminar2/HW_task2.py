ba04f6a96313e93120eec03be1ba3993f6781fda
Ivan <coltwalker@mail.ru>
2022-12-15 20:58:46 +0500

    wha

--- Seminar2/HW_task2.py
+++ Seminar2/HW_task2.py
@@ -5,10 +5,10 @@
 #     - пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
 
 
-N = int(input("Введите число "))
+n = int(input("Введите число "))
 
 p = 1
 
-for i in range(1, N+1):
+for i in range(1, n+1):
     p *= i
     print(p, end=" ")
